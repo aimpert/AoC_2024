98a9d6ec7f55fd813fd90f9d69d8a41565398a66
aimpert <aleximpert@gmail.com>
2024-12-08 16:43:36 -0800

    part 2!

--- day8/part2.cpp
+++ day8/part2.cpp
@@ -3,6 +3,7 @@
 #include "../include/AoC.h"
 #include <unordered_map>
 #include <unordered_set>
+#include <iterator>
 
 using namespace std;
 
@@ -36,21 +37,26 @@ int main () {
 
 
     unordered_set<pair<int, int>, pair_hash> antinodes;
-
+    int count = 0;
     for (const auto& [key, positions] : att) {
         for (size_t i = 0; i < positions.size(); i++) {
             for (size_t j = 0; j < positions.size(); j++) {
                 if (j == i) continue;
                 const auto& [row1, col1] = positions[i]; 
-                const auto& [row2, col2] = positions[j]; 
-                pair<int, int> antinode = {(row2 - row1) + row2, (col2 - col1) + col2};
-                if (antinode.first < 0 || antinode.first >= max_row || antinode.second < 0 || antinode.second >= max_col) continue;
-                else antinodes.insert(antinode);
+                const auto& [row2, col2] = positions[j];
+                antinodes.insert({row1, col1});
+                pair<int, int> distance = {(row2 - row1), (col2 - col1)};
+                pair<int, int> antinode = {row2 + distance.first, col2 + distance.second};
+                while (antinode.first >= 0 && antinode.first < max_row && antinode.second >= 0 && antinode.second < max_col) {
+                        antinodes.insert(antinode);
+                        antinode = {antinode.first + distance.first, antinode.second + distance.second};
+
+                    }
+                }
             }
         }
-    }
 
-    cout << antinodes.size() << endl;
+    cout << antinodes.size() + count << endl;
     input.close();
     return 0;
 }
